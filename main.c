e549c107db9315ab161d2375c01bd916145241e9
ziotonak <ziotonak@gmail.com>
2023-12-28 02:23:31 +0100

    Add card struct

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int T;
+typedef struct {
+    char f, s;
+} card_t;
 
 typedef struct {
     size_t length, capacity;
-    T *array, *front, *rear;
+    card_t *array, *front, *rear;
 } deque_t;
 
 int deque_init(deque_t *deque, size_t capacity) {
     deque->length = 0;
     deque->capacity = capacity;
-    deque->array = calloc(capacity, sizeof(T));
+    deque->array = calloc(capacity, sizeof(card_t));
     if (deque->array == NULL)
         return -1;
     deque->front = deque ->rear = deque->array;
@@ -22,16 +24,16 @@ void deque_free(deque_t *deque) {
     free(deque->array);
 }
 
-T *deque_at(deque_t *deque, size_t index) {
+card_t *deque_at(deque_t *deque, size_t index) {
     if (index >= deque->length)
         return NULL;
-    T *ptr = deque->front + index;
+    card_t *ptr = deque->front + index;
     if (ptr >= deque->array + deque->capacity)
         ptr -= deque->capacity;
     return ptr;
 }
 
-void deque_push_front(deque_t *deque, T value) {
+void deque_push_front(deque_t *deque, card_t value) {
     if (deque->length == deque->capacity)
         return; // overflow
     if (deque->length) {
@@ -43,7 +45,7 @@ void deque_push_front(deque_t *deque, T value) {
     *deque->front = value;
 }
 
-void deque_push_rear(deque_t *deque, T value) {
+void deque_push_rear(deque_t *deque, card_t value) {
     if (deque->length == deque->capacity)
         return; // overflow
     if (deque->length) {
@@ -60,13 +62,16 @@ int main() {
     deque_init(&deque, 15);
 
     for (int i=0; i<200; ++i) {
-        if (i & 1) deque_push_rear(&deque, i);
-        else deque_push_front(&deque, i);
+        card_t card = {i % 6, 6 - i % 6};
+        if (i & 1) deque_push_rear(&deque, card);
+        else deque_push_front(&deque, card);
     }
 
     if (deque.length) {
-        for (size_t i=0; i<deque.length; ++i)
-            printf("%d ", *deque_at(&deque, i));
+        for (size_t i=0; i<deque.length; ++i) {
+            card_t card = *deque_at(&deque, i);
+            printf("[%d|%d] ", card.f, card.s);
+        }
         putchar('\n');
     }
 
